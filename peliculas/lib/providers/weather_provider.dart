9617d74566a57b763bd1a72f5f250f5d957caf4c
Carlos Flores <carlosdelgado_09@hotmail.com>
2022-07-27 18:13:37 -0500

    api commit
    i add the new api.

--- peliculas/lib/providers/weather_provider.dart
+++ peliculas/lib/providers/weather_provider.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+//http://api.weatherapi.com/v1/forecast.json?key=a1fc9e6c652b4edfaab143006222507&q=Durango&days=5&aqi=no&alerts=no
 class WeatherProvider extends ChangeNotifier {
+
+  String _key = "a1fc9e6c652b4edfaab143006222507";
+  String _baseURL = "api.weatherapi.com";
+  String _q = "Durango";
+  String _days = "5";
+  String _aqi = "no";
+  String _alerts = "no";
 
   WeatherProvider() {
     print("WeatherProvider init");
@@ -8,8 +18,21 @@ class WeatherProvider extends ChangeNotifier {
     this.getOnDisplayWeather();
   }
 
-  getOnDisplayWeather() {
-    print("getOnDisplayWeather");
+  getOnDisplayWeather() async {
+    var url =
+      Uri.https(this._baseURL, 'v1/forecast.json', {
+        "key": _key,
+        "q": _q,
+        "days": _days,
+        "aqi": _aqi,
+        "alerts": _alerts
+      });
+
+  // Await the http get response, then decode the json-formatted response.
+  final response = await http.get(url);
+  final Map<String, dynamic> apiDecoded = json.decode(response.body);
+  
+  print(apiDecoded["days"]);
   }
 
 }
--- peliculas/lib/widgets/forecast_cards.dart
+++ peliculas/lib/widgets/forecast_cards.dart
@@ -9,7 +9,49 @@ class ForecastCarsd extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 30),
       width: double.infinity,
       height: 180,
-      color: Colors.red,
+      child: ListView.builder(
+        itemCount: 4,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, int idex) => _ForecastCard(),
+      ),
+    );
+    
+  }
+}
+
+class _ForecastCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric( horizontal: 10),
+      width: 110,
+      height: 100,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage("assets/no-image.jpg"), 
+              image: AssetImage("assets/profile.jpg"),
+              height: 140,
+              width: 100,
+            ),
+          ),
+          SizedBox(height: 1),
+          Text(
+            "Day",
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "Temp",
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ]
+      ),
+
     );
     
   }
